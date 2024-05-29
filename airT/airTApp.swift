c700fd0348572e18ba1624ea9d34d027133d314e
Nick Martynov <kolyapusya@icloud.com>
2024-05-29 17:34:55 +0300

    Initial Commit

---
+++ airT/airTApp.swift
@@ -0,0 +1,17 @@
+//
+//  airTApp.swift
+//  airT
+//
+//  Created by Николай Мартынов on 29.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct airTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
